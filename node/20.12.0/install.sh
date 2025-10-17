329d89149a266b90c200b192c38ace44e1f03e4d
lteti77 <lteti77@yahoo.it>
2025-10-17 11:11:00 +0200

    Update Node.js installation script to use BASE_URL

--- node/20.12.0/install.sh
+++ node/20.12.0/install.sh
@@ -4,13 +4,14 @@ set -e
 VERSION="20.12.0"
 INSTALL_DIR="$VFOX_HOME/tools/node/$VERSION"
 ARCH="linux-x64"
+BASE_URL="nodejs.org/dist"
 
 echo "Installing Node.js $VERSION..."
 
 mkdir -p "$INSTALL_DIR"
 
-# Scarica da Nexus (dove hai caricato i tar.gz)
-curl -fsSL -H "Authorization: Basic bm9kZWpzLXJlcG8tdXNlcjpub2RlanMtcmVwby11c2Vy" "https://nexus.utenze.bankit.it/nexus/repository/nodejs-proxy/v${VERSION}/node-v${VERSION}-${ARCH}.tar.gz" -o /tmp/node.tar.gz
+# Scarica i tar.gz
+curl -fsSL "https://${BASE_URL}/v${VERSION}/node-v${VERSION}-${ARCH}.tar.gz" -o /tmp/node.tar.gz
 
 tar -xzf /tmp/node.tar.gz -C "$INSTALL_DIR" --strip-components=1
 rm /tmp/node.tar.gz
